9b47322b2a9809077faeb307de04ba8e16e5ad3a
Martin Krause <martin89@gmx.net>
2021-02-14 22:39:43 +0100

    Added SettingsViewModel and required commands

---
+++ MyCoach/MyCoach/ViewModel/Commands/ResetSettingsCommand.cs
@@ -0,0 +1,37 @@
+﻿using MyCoach.DataHandling;
+using MyCoach.DataHandling.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyCoach.ViewModel.Commands
+{
+    public class ResetSettingsCommand : ICommand
+    {
+        private SettingsViewModel settingsViewModel;
+
+        public ResetSettingsCommand(SettingsViewModel vm)
+        {
+            this.settingsViewModel = vm;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.settingsViewModel.HasUnsavedSettings;
+        }
+
+        public void Execute(object parameter)
+        {
+            this.settingsViewModel.LoadSettingsBuffer();
+        }
+    }
+}
--- MyCoach/MyCoach/ViewModel/SettingsViewModel.cs
+++ MyCoach/MyCoach/ViewModel/SettingsViewModel.cs
@@ -1,12 +1,211 @@
-﻿using System;
+﻿using MyCoach.DataHandling;
+using MyCoach.DataHandling.DataTransferObjects;
+using MyCoach.Defines;
+using MyCoach.ViewModel.Commands;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MyCoach.ViewModel
 {
     public class SettingsViewModel : BaseViewModel
     {
+        public SettingsViewModel()
+        {
+            this.Settings = new Settings();
+            this.LoadSettingsBuffer();
+            this.SaveSettingsCommand = new SaveSettingsCommand(this);
+        }
+
+        public bool HasUnsavedSettings { get; set; }
+
+        public ICommand SaveSettingsCommand { get; }
+
+        public Settings Settings { get; private set; }
+
+        public ExerciseSchedulingRepetitionPermission Permission
+        {
+            get => this.Settings.Permission;
+
+            set
+            {
+                if (this.Settings.Permission == value)
+                {
+                    return;
+                }
+
+                this.Settings.Permission = value;
+                this.InvokePropertyChanged();
+            }
+        }
+
+        public ushort RepeatsRound1
+        {
+            get => this.Settings.RepeatsRound1;
+
+            set
+            {
+                if (this.Settings.RepeatsRound1 == value)
+                {
+                    return;
+                }
+
+                this.Settings.RepeatsRound1 = value;
+                this.InvokePropertyChanged();
+            }
+        }
+
+        public ushort RepeatsRound2
+        {
+            get => this.Settings.RepeatsRound2;
+
+            set
+            {
+                if (this.Settings.RepeatsRound2 == value)
+                {
+                    return;
+                }
+
+                this.Settings.RepeatsRound2 = value;
+                this.InvokePropertyChanged();
+            }
+        }
+
+        public ushort RepeatsRound3
+        {
+            get => this.Settings.RepeatsRound3;
+
+            set
+            {
+                if (this.Settings.RepeatsRound3 == value)
+                {
+                    return;
+                }
+
+                this.Settings.RepeatsRound3 = value;
+                this.InvokePropertyChanged();
+            }
+        }
+
+        public ushort RepeatsRound4
+        {
+            get => this.Settings.RepeatsRound4;
+
+            set
+            {
+                if (this.Settings.RepeatsRound4 == value)
+                {
+                    return;
+                }
+
+                this.Settings.RepeatsRound4 = value;
+                this.InvokePropertyChanged();
+            }
+        }
+
+        public ushort ScoresRound1
+        {
+            get => this.Settings.ScoresRound1;
+
+            set
+            {
+                if (this.Settings.ScoresRound1 == value)
+                {
+                    return;
+                }
+
+                this.Settings.ScoresRound1 = value;
+                this.InvokePropertyChanged();
+            }
+        }
+
+        public ushort ScoresRound2
+        {
+            get => this.Settings.ScoresRound2;
+
+            set
+            {
+                if (this.Settings.ScoresRound2 == value)
+                {
+                    return;
+                }
+
+                this.Settings.ScoresRound2 = value;
+                this.InvokePropertyChanged();
+            }
+        }
+
+        public ushort ScoresRound3
+        {
+            get => this.Settings.ScoresRound3;
+
+            set
+            {
+                if (this.Settings.ScoresRound3 == value)
+                {
+                    return;
+                }
+
+                this.Settings.ScoresRound3 = value;
+                this.InvokePropertyChanged();
+            }
+        }
+
+        public ushort ScoresRound4
+        {
+            get => this.Settings.ScoresRound4;
+
+            set
+            {
+                if (this.Settings.ScoresRound4 == value)
+                {
+                    return;
+                }
+
+                this.Settings.ScoresRound4 = value;
+                this.InvokePropertyChanged();
+            }
+        }
+
+        public void LoadSettingsBuffer()
+        {
+            var savedSettings = DataInterface.GetInstance().GetDataTransferObjects<Settings>()?.FirstOrDefault();
+
+            if (savedSettings == null)
+            {
+                this.SetDefaultSettings();
+                return;
+            }
+
+            this.Settings = (Settings)savedSettings.Clone();
+        }
+
+        public void SaveSettings()
+        {
+            ObservableCollection<Settings> settingsToSave = new ObservableCollection<Settings> { this.Settings };
+            DataInterface.GetInstance().SetDataTransferObjects<Settings>(settingsToSave);
+        }
+
+        public void SetDefaultSettings()
+        {
+            this.Settings = new Settings
+            {
+                Permission = ExerciseSchedulingRepetitionPermission.NotPreferred,
+                RepeatsRound1 = 100,
+                RepeatsRound2 = 75,
+                RepeatsRound3 = 60,
+                RepeatsRound4 = 50,
+                ScoresRound1 = 100,
+                ScoresRound2 = 100,
+                ScoresRound3 = 100,
+                ScoresRound4 = 100
+            };
+
+            this.SaveSettings();
+        }
     }
 }
