4273ca8fb680515b5c7270534bdf4779ef0a9b36
Martin Krause <martin89@gmx.net>
2021-04-14 21:18:29 +0200

    Added current month to ViewTrainingScheduleView and -ViewModel and made visibility of months of time based training schedule dependent on training schedule settings

--- MyCoach/MyCoach/ViewModel/ViewTrainingScheduleViewModel.cs
+++ MyCoach/MyCoach/ViewModel/ViewTrainingScheduleViewModel.cs
@@ -12,12 +12,15 @@ namespace MyCoach.ViewModel
 {
     public class ViewTrainingScheduleViewModel : BaseViewModel
     {
+        private MonthViewModel currentMonthViewModel;
+        private bool timeBasedScheduleElementsVisible;
+
         public ViewTrainingScheduleViewModel()
         {
             this.MonthViewModelsInTimeBasedSchedule = new ObservableCollection<MonthViewModel>();
             this.Months = DataInterface.GetInstance().GetDataTransferObjects<Month>();
             this.TrainingSchedule = DataInterface.GetInstance().GetDataTransferObjects<TrainingSchedule>().FirstOrDefault();
-            this.PopulateMonthViewModels();
+            this.UpdateMonthViewModels();
         }
 
         public TrainingSchedule TrainingSchedule { get; }
@@ -26,37 +29,83 @@ namespace MyCoach.ViewModel
 
         public ObservableCollection<Month> Months { get; }
 
-        public MonthViewModel MonthViewModelCurrent { get; private set; }
+        public MonthViewModel CurrentMonthViewModel
+        {
+            get => this.currentMonthViewModel; 
+            
+            private set
+            {
+                if (this.currentMonthViewModel == value)
+                {
+                    return;
+                }
+
+                this.currentMonthViewModel = value;
+                this.InvokePropertyChanged();
+            }
+        }
 
         public ObservableCollection<MonthViewModel> MonthViewModelsInTimeBasedSchedule { get; }
 
-        private void PopulateMonthViewModels()
+        public bool TimeBasedScheduleElementsVisible
         {
-            var currentMonth = this.Months?.Where(m => m.Number == MonthNumber.Current).FirstOrDefault();
+            get => this.timeBasedScheduleElementsVisible;
 
-            if (currentMonth != null)
+            private set
             {
-                this.MonthViewModelCurrent = new MonthViewModel(this.CalculateStartDate(currentMonth), currentMonth);
-            }
+                if (this.timeBasedScheduleElementsVisible == value)
+                {
+                    return;
+                }
 
+                this.timeBasedScheduleElementsVisible = value;
+                this.InvokePropertyChanged();
+            }
+        }
+
+        private void UpdateMonthViewModels()
+        {
+            this.UpdateCurrentMonthViewModel();
             this.MonthViewModelsInTimeBasedSchedule.Clear();
 
             foreach (var month in this.Months)
             {
-                if (month.Number == MonthNumber.Current)
+                if (month.Number == MonthNumber.Current || (int)month.Number > this.TrainingSchedule.Duration)
                 {
                     continue;
                 }
 
-                this.MonthViewModelsInTimeBasedSchedule.Add(new MonthViewModel(this.CalculateStartDate(month), month));
+                this.MonthViewModelsInTimeBasedSchedule.Add(new MonthViewModel(this.GetStartDate(month), month));
             }
         }
 
-        private DateTime CalculateStartDate(Month month)
+        private void UpdateCurrentMonthViewModel()
+        {
+            var currentMonth = this.Months?.Where(m => m.Number == MonthNumber.Current).FirstOrDefault();
+
+            if (currentMonth == null)
+            {
+                return;
+            }
+
+            var currentMonthInTimeBasedSchedule = this.Months.Where(
+                m => this.GetStartDate(m) == this.GetStartDate(currentMonth) 
+                    && m.Number != MonthNumber.Current).FirstOrDefault();
+
+            if (currentMonthInTimeBasedSchedule != null
+                && this.TrainingSchedule.ScheduleType == ScheduleType.TimeBased)
+            {
+                currentMonth = currentMonthInTimeBasedSchedule;
+            }
+
+            this.CurrentMonthViewModel = new MonthViewModel(this.GetStartDate(currentMonth), currentMonth);
+        }
+
+        private DateTime GetStartDate(Month month)
         {
             if (month.Number == MonthNumber.Current)
             {
-                return DateTime.Now;
+                return new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
             }
 
             return this.TrainingSchedule.StartMonth.AddMonths((int)month.Number - 1);
