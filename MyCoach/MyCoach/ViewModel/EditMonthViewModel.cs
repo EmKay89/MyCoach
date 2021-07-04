1e98eae28f0ee282d5774e0b57ad30c86c0b092d
Martin Krause <martin89@gmx.net>
2021-07-04 14:11:31 +0200

    Added logic for EditMonthViewModel and added EditMonthViewModelTests

--- MyCoach/MyCoach/ViewModel/EditMonthViewModel.cs
+++ MyCoach/MyCoach/ViewModel/EditMonthViewModel.cs
@@ -1,6 +1,9 @@
-﻿using MyCoach.DataHandling.DataTransferObjects;
+﻿using MyCoach.DataHandling;
+using MyCoach.DataHandling.DataTransferObjects;
+using MyCoach.Defines;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +12,116 @@ namespace MyCoach.ViewModel
 {
     public class EditMonthViewModel : BaseViewModel
     {
+        private Month month;
+        private IReadOnlyCollection<Category> categories;
+
         public EditMonthViewModel(Month month)
         {
+            this.month = month;
+            this.categories = DataInterface.GetInstance().GetData<Category>();
+        }
 
+        public bool Category1ItemsVisible
+        {
+            get => categories.FirstOrDefault(c => c.ID == ExerciseCategory.Category1)?.Active == true;
+        }
+
+        public ushort Category1Goal
+        {
+            get => this.month.Category1Goal;
+
+            set => this.month.Category1Goal = value;
+        }
+
+        public bool Category2ItemsVisible
+        {
+            get => categories.FirstOrDefault(c => c.ID == ExerciseCategory.Category2)?.Active == true;
+        }
+
+        public ushort Category2Goal
+        {
+            get => this.month.Category2Goal;
+
+            set => this.month.Category2Goal = value;
+        }
+
+        public bool Category3ItemsVisible
+        {
+            get => categories.FirstOrDefault(c => c.ID == ExerciseCategory.Category3)?.Active == true;
+        }
+
+        public ushort Category3Goal
+        {
+            get => this.month.Category3Goal;
+
+            set => this.month.Category3Goal = value;
+        }
+
+        public bool Category4ItemsVisible
+        {
+            get => categories.FirstOrDefault(c => c.ID == ExerciseCategory.Category4)?.Active == true;
+        }
+
+        public ushort Category4Goal
+        {
+            get => this.month.Category4Goal;
+
+            set => this.month.Category4Goal = value;
+        }
+
+        public bool Category5ItemsVisible
+        {
+            get => categories.FirstOrDefault(c => c.ID == ExerciseCategory.Category5)?.Active == true;
+        }
+
+        public ushort Category5Goal
+        {
+            get => this.month.Category5Goal;
+
+            set => this.month.Category5Goal = value;
+        }
+
+        public bool Category6ItemsVisible
+        {
+            get => categories.FirstOrDefault(c => c.ID == ExerciseCategory.Category6)?.Active == true;
+        }
+
+        public ushort Category6Goal
+        {
+            get => this.month.Category6Goal;
+
+            set => this.month.Category6Goal = value;
+        }
+
+        public bool Category7ItemsVisible
+        {
+            get => categories.FirstOrDefault(c => c.ID == ExerciseCategory.Category7)?.Active == true;
+        }
+
+        public ushort Category7Goal
+        {
+            get => this.month.Category7Goal;
+
+            set => this.month.Category7Goal = value;
+        }
+
+        public bool Category8ItemsVisible
+        {
+            get => categories.FirstOrDefault(c => c.ID == ExerciseCategory.Category8)?.Active == true;
+        }
+
+        public ushort Category8Goal
+        {
+            get => this.month.Category8Goal;
+
+            set => this.month.Category8Goal = value;
+        }
+
+        public string MonthName
+        {
+            get => this.month.Number == MonthNumber.Current 
+                ? string.Empty 
+                : this.month.StartDate.ToString("y", CultureInfo.CurrentCulture);
         }
     }
 }
---
+++ MyCoach/MyCoachTests/ViewModel/EditMonthViewModelTest.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCoach.DataHandling.DataTransferObjects;
+using MyCoach.Defines;
+using MyCoach.ViewModel;
+using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace MyCoachTests.ViewModel
+{
+    [TestClass]
+    public class EditMonthViewModelTest : ViewModelTestBase
+    {
+        #region Initialization and Cleanup
+
+        private EditMonthViewModel sut;
+        private Month month;
+
+        [TestInitialize]
+        public void Init()
+        {
+            base.Initialize();
+            this.SetupMonth();
+            this.sut = new EditMonthViewModel(month);
+            this.sut.PropertyChanged +=
+                (object sender, PropertyChangedEventArgs e) => { this.PropertyChangedEvents.Add(e.PropertyName); };
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            base.CleanupTestBase();
+        }
+
+        #endregion
+
+        #region Construction and Properties Tests
+
+        [TestMethod]
+        public void Construction_PropertiesSetUpCorrectly()
+        {
+            Assert.AreEqual(100, this.sut.Category1Goal);
+            Assert.AreEqual(200, this.sut.Category2Goal);
+            Assert.AreEqual(300, this.sut.Category3Goal);
+            Assert.AreEqual(400, this.sut.Category4Goal);
+            Assert.AreEqual(500, this.sut.Category5Goal);
+            Assert.AreEqual(600, this.sut.Category6Goal);
+            Assert.AreEqual(700, this.sut.Category7Goal);
+            Assert.AreEqual(800, this.sut.Category8Goal);
+
+            Assert.AreEqual(this.Categories.First(c => c.ID == ExerciseCategory.Category1).Active,
+                this.sut.Category1ItemsVisible);
+            Assert.AreEqual(this.Categories.First(c => c.ID == ExerciseCategory.Category2).Active,
+                this.sut.Category2ItemsVisible);
+            Assert.AreEqual(this.Categories.First(c => c.ID == ExerciseCategory.Category3).Active,
+                this.sut.Category3ItemsVisible);
+            Assert.AreEqual(this.Categories.First(c => c.ID == ExerciseCategory.Category4).Active,
+                this.sut.Category4ItemsVisible);
+            Assert.AreEqual(this.Categories.First(c => c.ID == ExerciseCategory.Category5).Active,
+                this.sut.Category5ItemsVisible);
+            Assert.AreEqual(this.Categories.First(c => c.ID == ExerciseCategory.Category6).Active,
+                this.sut.Category6ItemsVisible);
+            Assert.AreEqual(this.Categories.First(c => c.ID == ExerciseCategory.Category7).Active,
+                this.sut.Category7ItemsVisible);
+            Assert.AreEqual(this.Categories.First(c => c.ID == ExerciseCategory.Category8).Active,
+                this.sut.Category8ItemsVisible);
+
+            Assert.AreEqual("November 1989", this.sut.MonthName);
+
+            this.sut = new EditMonthViewModel(new Month(){ Number = MonthNumber.Current });
+
+            Assert.AreEqual(string.Empty, this.sut.MonthName);
+        }
+
+        [TestMethod]
+        public void SettingGoals_PropertiesOfMonthUpdatedCorrectly()
+        {
+            this.sut.Category1Goal = (ushort)(this.sut.Category1Goal * 10);
+            this.sut.Category2Goal = (ushort)(this.sut.Category2Goal * 10);
+            this.sut.Category3Goal = (ushort)(this.sut.Category3Goal * 10);
+            this.sut.Category4Goal = (ushort)(this.sut.Category4Goal * 10);
+            this.sut.Category5Goal = (ushort)(this.sut.Category5Goal * 10);
+            this.sut.Category6Goal = (ushort)(this.sut.Category6Goal * 10);
+            this.sut.Category7Goal = (ushort)(this.sut.Category7Goal * 10);
+            this.sut.Category8Goal = (ushort)(this.sut.Category8Goal * 10);
+
+            Assert.AreEqual(1000, this.month.Category1Goal);
+            Assert.AreEqual(2000, this.month.Category2Goal);
+            Assert.AreEqual(3000, this.month.Category3Goal);
+            Assert.AreEqual(4000, this.month.Category4Goal);
+            Assert.AreEqual(5000, this.month.Category5Goal);
+            Assert.AreEqual(6000, this.month.Category6Goal);
+            Assert.AreEqual(7000, this.month.Category7Goal);
+            Assert.AreEqual(8000, this.month.Category8Goal);
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        private void SetupMonth()
+        {
+            this.month = new Month()
+            {
+                Number = MonthNumber.Month1,
+                Category1Goal = 100,
+                Category2Goal = 200,
+                Category3Goal = 300,
+                Category4Goal = 400,
+                Category5Goal = 500,
+                Category6Goal = 600,
+                Category7Goal = 700,
+                Category8Goal = 800,
+                StartDate = new DateTime(1989, 11, 01)
+            };
+        }
+
+        #endregion
+    }
+}
