0dee240ab62f1bdf924ac1713fe7449feb7c2d1a
Martin Krause <martin89@gmx.net>
2021-08-26 18:23:50 +0200

    Added some classes as base for training generation (WIP)

---
+++ MyCoach/MyCoach/ViewModel/TrainingGeneration/TrainingExerciseViewModel.cs
@@ -0,0 +1,73 @@
+﻿using MyCoach.DataHandling.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCoach.ViewModel.TrainingGeneration
+{
+    public class TrainingExerciseViewModel : BaseViewModel
+    {        
+        private readonly Exercise exercise;
+        private bool completed;
+
+        public TrainingExerciseViewModel(Exercise exercise)
+        {
+            this.exercise = exercise;
+        }
+
+        public const string UNKNOWN_EXERCISE_NAME = "unbekannte Übung";
+
+        public event EventHandler TrainingExerciseCompleted;
+
+        public string Text
+        {
+            get => GetText();
+        }
+
+        public bool Completed
+        {
+            get => this.completed;
+
+            set
+            {
+                if (value == this.completed)
+                {
+                    return;
+                }
+
+                this.completed = value;
+                this.InvokePropertyChanged();
+            }
+        }
+
+        public string Info
+        {
+            get => this.exercise.Info;
+        }
+
+        public double Multiplier { get; set; } = 1.0;
+
+        public uint GetScores()
+        {
+            return this.completed ? (uint)Math.Round(this.exercise.Scores * this.Multiplier) : 0;
+        }
+
+        private string GetText()
+        {
+            var text = ((uint)(this.exercise.Count * Multiplier)).ToString();
+
+            if (this.exercise.Unit != null && this.exercise.Unit != string.Empty)
+            {
+                text = string.Concat(text, " ", this.exercise.Unit);
+            }
+
+            text = this.exercise.Name != null && this.exercise.Name != string.Empty
+                ? string.Concat(text, " ", this.exercise.Name)
+                : string.Concat(text, " ", UNKNOWN_EXERCISE_NAME);            
+
+            return text;
+        }
+    }
+}
---
+++ MyCoach/MyCoach/ViewModel/TrainingGeneration/TrainingGenerator.cs
@@ -0,0 +1,42 @@
+﻿using MyCoach.DataHandling;
+using MyCoach.DataHandling.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCoach.ViewModel.TrainingGeneration
+{
+    public static class TrainingGenerator
+    {
+        public static Training CreateTraining(TrainingSettings trainingSettings)
+        {
+            var training = new Training();
+            var globalSettings = DataInterface.GetInstance().GetData<Settings>().FirstOrDefault();
+
+
+
+            return training;
+        }
+
+        public static double DetermineMultiplierForRound(
+            int round, 
+            Settings globalSettings)
+        {
+            switch (round)
+            {
+                case 1:
+                    return 1.0 * globalSettings.ScoresRound1 / 100;
+                case 2:
+                    return 1.0 * globalSettings.ScoresRound2 / 100;
+                case 3:
+                    return 1.0 * globalSettings.ScoresRound3 / 100;
+                case 4:
+                    return 1.0 * globalSettings.ScoresRound4 / 100;
+                default:
+                    return 1.0;                    
+            }
+        }
+    }
+}
