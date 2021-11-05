54cad8c30f668eefc7b36bf087c4277d619a5d0f
Martin Krause <martin89@gmx.net>
2021-11-05 10:26:06 +0100

    Small cleanup

--- MyCoach/MyCoach/View/DatePickerCalender.cs
+++ MyCoach/MyCoach/View/DatePickerCalender.cs
@@ -169,8 +169,7 @@ namespace MyCoach.View
         /// </summary>
         private static void DropDownButtonPreviewMouseUp(object sender, MouseButtonEventArgs e)
         {
-            var fe = sender as FrameworkElement;
-            if (fe == null) return;
+            if (!(sender is FrameworkElement fe)) return;
 
             var datePicker = fe.TryFindParent<DatePicker>();
             if (datePicker == null || datePicker.SelectedDate == null) return;
@@ -256,9 +255,8 @@ namespace MyCoach.View
 
             public static DateTime? StringToDateTime(DatePicker datePicker, string formatStr, string dateStr)
             {
-                DateTime date;
                 var canParse = DateTime.TryParseExact(dateStr, formatStr, CultureInfo.CurrentCulture,
-                                                      DateTimeStyles.None, out date);
+                                                      DateTimeStyles.None, out DateTime date);
 
                 if (!canParse)
                     canParse = DateTime.TryParse(dateStr, CultureInfo.CurrentCulture, DateTimeStyles.None, out date);
@@ -289,8 +287,7 @@ namespace MyCoach.View
             if (parentObject == null) return null;
 
             //check if the parent matches the type we're looking for
-            T parent = parentObject as T;
-            if (parent != null)
+            if (parentObject is T parent)
             {
                 return parent;
             }
@@ -315,19 +312,16 @@ namespace MyCoach.View
             if (child == null) return null;
 
             //handle content elements separately
-            ContentElement contentElement = child as ContentElement;
-            if (contentElement != null)
+            if (child is ContentElement contentElement)
             {
                 DependencyObject parent = ContentOperations.GetParent(contentElement);
                 if (parent != null) return parent;
 
-                FrameworkContentElement fce = contentElement as FrameworkContentElement;
-                return fce != null ? fce.Parent : null;
+                return contentElement is FrameworkContentElement fce ? fce.Parent : null;
             }
 
             //also try searching for parent in framework elements (such as DockPanel, etc)
-            FrameworkElement frameworkElement = child as FrameworkElement;
-            if (frameworkElement != null)
+            if (child is FrameworkElement frameworkElement)
             {
                 DependencyObject parent = frameworkElement.Parent;
                 if (parent != null) return parent;
